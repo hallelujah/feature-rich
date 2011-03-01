e315b9d383a10ef5df74692cb99e281d9c86592a
hallelujah <hery@rails-royce.org>
2011-03-01 16:04:35 +0100

    Fix init

--- init.rb
+++ init.rb
@@ -2,8 +2,4 @@ require 'feature-rich'
 # Ok let's go
 config.after_initialize do
   FeatureRich::Engine.load!
-  FeatureRich::Engine.config.config_file ||= Rails.root.join('config/feature-rich.rb').to_s
-  FeatureRich::Engine.run do
-    eval(File.read(FeatureRich::Engine.config.config_file))
-  end
 end
